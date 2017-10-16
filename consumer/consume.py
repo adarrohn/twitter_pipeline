855ae43497dc5f8b086fc493c3aeab4610fc3fa1
Andrew Darrohn <andrew.darrohn@smashdocs.net>
2017-10-16 14:58:09 +0200

    restructured consumer and producer code to improve error handling and cleans up runtime initialization

--- consumer/consume.py
+++ consumer/consume.py
@@ -1,57 +1,110 @@
-# Copyright 2017 Andrew Darrohn
+# Copyright (C) 2017 Andrew Darrohn - All Rights Reserved
 
 import time
 import datetime
 import json
 
 from kafka import KafkaConsumer
+from kafka.errors import NoBrokersAvailable
 
 
-def consume_tweets():
+class Consumer:
     """
-    Calculates the sentiment score for the given tweet and returns the dict payload to be sent to kafka
+     A KafkaConsumer wrapper that consumes messages and writes them to disk
 
-    :param dict tweet: a tweet as a dict
-    :return: a dict containing the keys version, twitter_id, created_timestamp, original_tweet, retweet_count and
-    its sentiment score
+    :ivar str topic: the topic under which the tweets will be sent to
+    :ivar `KafkaConsumer` consumer: the KafkaProducer reference initialized with a json utf8 serializer
+    """
 
+    def __init__(self, topic, output_path):
+        """
+        :param str topic: sets the kafka topic where the consumer will read from
+        :param str output_path: file path that the consumer writes to
+        """
+
+        self.topic = topic
+        self.output_path = output_path
+
+        try:
+            self.consumer = KafkaConsumer(topic,
+                                          bootstrap_servers=['kafka'],
+                                          value_deserializer=lambda x: json.loads(x.decode('utf8')))
+        except NoBrokersAvailable:
+            time.sleep(5)
+            self.__init__(topic, output_path)
+
+    def consume(self):
+        """
+        Event loop which consumes messages from the KafkaConsumer and writes them to disk
+        """
+
+        for message in self.consumer:
+            with open(self.output_path, 'w') as f:
+                processed_message = pack_message(message)
+
+                if processed_message:
+                    f.write(str(processed_message))
+                    f.write('\n')
+
+
+def pack_message(message):
+    """
+    Builds a dict with the following keys from a given kafka message:
+
+    Keys:
+        version
+        twitter_id
+        created_timestamp
+        original_text
+        retweet_count
+        sentiment_score
+
+    :param dict message: dict containing the Kafka message
+
+    :return: a dict of the keys with calculated sentient score or None in the case of a KeyError
     :rtype: dict
     """
+    try:
+        return {'version': 'v1',
+                'twitter_id': message.value['user']['id_str'],
+                'created_timestamp': datetime.datetime.now(datetime.timezone.utc).timestamp(),
+                'original_text': message.value['text'],
+                'retweet_count': message.value['retweet_count'],
+                'sentiment_score': calculate_sentiment_score(message.value['text'])}
 
-    def calculate_sentiment_score(tweet_text):
-        """
-        Calculates a sentiment score for the given tweet_text based on the following parameters:
+    except KeyError:
+        return None
 
-        :param str tweet_text: a tweets content as a str
-        :return: the calculated sentiment_score for the given text
 
-        :rtype: float
-        """
+def calculate_sentiment_score(tweet_text):
+    """
+    Calculates a sentiment score for the given tweet_text based on the following parameters:
 
-        smile_score = .2 if ':)' in tweet_text else 0.0
-        frown_score = -.2 if ':(' in tweet_text else 0.0
-        bad_score = tweet_text.lower().count('bad') * -.01
-        exclamation_multiplier = 2 if '!' in tweet_text else 1.0
+    :param str tweet_text: a tweets content as a str
 
-        return sum([smile_score,
-                    frown_score,
-                    bad_score]) * exclamation_multiplier
+    :return: the calculated sentiment_score for the given text
+    :rtype: float
+    """
 
-    consumer = KafkaConsumer('crypto',
-                             bootstrap_servers=['kafka'],
-                             value_deserializer=lambda x: json.loads(x.decode('utf8')))
+    smile_score = .2 if ':)' in tweet_text else 0.0
+    frown_score = -.2 if ':(' in tweet_text else 0.0
+    bad_score = tweet_text.lower().count('bad') * -.01
+    exclamation_multiplier = 2 if '!' in tweet_text else 1.0
 
-    for message in consumer:
-        print(
-            {'version': 'v1',
-             'twitter_id': message.value['user']['id_str'],
-             'created_timestamp': datetime.datetime.now(datetime.timezone.utc).timestamp(),
-             'original_text': message.value['text'],
-             'retweet_count': message.value['retweet_count'],
-             'sentiment_score': calculate_sentiment_score(message.value['text'])})
+    return sum([smile_score,
+                frown_score,
+                bad_score]) * exclamation_multiplier
+
+
+def main():
+    """
+    Consumes messages from kafka which are in the topic "crypto" anbd writes them to disk
+
+    """
+
+    consumer = Consumer('crypto', '/usr/local/data/consumer/consumer_out.txt')
+    consumer.consume()
 
 
 if __name__ == '__main__':
-    # change how this runs
-    time.sleep(5)
-    consume_tweets()
+    main()
--- producer/produce.py
+++ producer/produce.py
@@ -5,49 +5,76 @@ import json
 
 from twitter import OAuth, TwitterHTTPError, TwitterStream
 from kafka import KafkaProducer
-from kafka.errors import KafkaError
+from kafka.errors import KafkaError, NoBrokersAvailable
 
 from settings import *
 
 
-def produce_tweets():
+class Producer:
+    """
+    A KafkaProducer wrapper that fetches tweets based on specific filters
+
+    :ivar str topic: the topic under which the tweets will be sent to
+    :ivar `TwitterStream` stream: the twitter stream object filtered on the filter_keywords
+    :ivar str filter_keywords: a csv string of keywords to filter tweets on
+    :ivar `KafkaProducer` producer: the KafkaProducer reference initialized with a json utf8 serializer
+    """
+
+    def __init__(self, topic, filter_on=[]):
+        """
+        :param str topic: sets the kafka topic where the producer will send to
+        :param list filter_on: a list of strings
+        """
+
+        self.topic = topic
+        self.stream = TwitterStream(auth=OAuth(twitter_access_token,
+                                               twitter_access_secret,
+                                               twitter_consumer_key,
+                                               twitter_consumer_secret))
+        self.filter_keywords = ','.join(filter_on)
+
+        try:
+            self.producer = KafkaProducer(bootstrap_servers=['kafka'],
+                                          value_serializer=lambda x: json.dumps(x).encode('utf8'))
+        except NoBrokersAvailable:
+            time.sleep(5)
+            self.__init__(topic, filter_on)
+
+    def fetch(self):
+        """
+        Event loop to fetch tweets with a 60 second wait time once the iter is exhausted before re-fetching
+
+        """
+
+        while True:
+            try:
+                filtered_stream_iter = self.stream.statuses.filter(track=self.filter_keywords)
+
+                for tweet in filtered_stream_iter:
+                    try:
+                        self.producer.send(self.topic,
+                                           tweet)
+
+                    except KafkaError as kafka_error:
+                        print(kafka_error)
+
+            except TwitterHTTPError as twitter_error:
+                print(twitter_error)
+
+            time.sleep(60)
+
+
+def main():
     """
     Connects to the twitter API and re-queries every 60 seconds for the given keywords and subsequently pushes the
-    procesed results to kafka
+    procesed results to kafka under the topic "crypto"
 
     """
 
-    oauth = OAuth(twitter_access_token,
-                  twitter_access_secret,
-                  twitter_consumer_key,
-                  twitter_consumer_secret)
-
-    producer = KafkaProducer(bootstrap_servers=['kafka'],
-                             value_serializer=lambda x: json.dumps(x).encode('utf8'))
-
-    stream = TwitterStream(auth=oauth)
-    tracking_keywords = ','.join(['bitcoin',
-                                  'crypto'])
-
-    while True:
-        try:
-            filtered_stream_iter = stream.statuses.filter(track=tracking_keywords)
-
-            for tweet in filtered_stream_iter:
-                try:
-                    producer.send('crypto',
-                                  tweet)
-
-                except KafkaError as kafka_error:
-                    print(kafka_error)
-
-        except TwitterHTTPError as twitter_error:
-            print(twitter_error)
-
-        time.sleep(60)
+    producer = Producer('crypto',
+                        filter_on=['bitcoin', 'crypto'])
+    producer.fetch()
 
 
 if __name__ == '__main__':
-    # change how this runs
-    time.sleep(5)
-    produce_tweets()
+    main()
